96288870a946e231dc91b05d2a951eb6ffbe14cb
RajSukhadiya21 <rajsukhadiya387@gmail.com>
2022-08-07 19:43:56 +0530

    This is my Practical-1

---
+++ src/main/kotlin/Practical-1.kt
@@ -0,0 +1,15 @@
+fun main()
+{
+    var x:Int=22
+    println("$x")
+    var z=39.5f
+    println("$z")
+    var y='R'
+    println("$y")
+    var a="Raj"
+    println("$a")
+    var b=true
+    println("$b")
+    var c=96.36
+    println("$c")
+}
